c72d5cda12a214dc4d2368ff222374a3ba554d19
Jacob Edwards <edwardsjacob1830@gmail.com>
2018-08-11 19:18:23 -0400

    final commit

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+SELECT * FROM burgers;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES("Big Tasty", "False");
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Despacito", "False");
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Chipotle Burger", "False");
